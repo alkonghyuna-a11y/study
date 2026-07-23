1299f27db053fb7bef148946a8576909e8ae5462
alkonghyuna-a11y <alkonghyuna@gmail.com>
2026-07-23 21:01:07 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE="study-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-512-maskable.png","./apple-touch-icon.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(cc=>cc.put(e.request,c)).catch(()=>{});return res;}).catch(()=>caches.match("./index.html"))));});
